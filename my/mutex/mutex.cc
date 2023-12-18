3a8c8704ff0da1f1042c505e790ed6421225015b
yzj <1790122327@qq.com>
2023-12-18 20:34:17 +0800

    基于pthread实现了条件变量

--- my/mutex/mutex.cc
+++ my/mutex/mutex.cc
@@ -3,7 +3,10 @@
 
 #include <iostream>
 
-
+/**
+ * @brief 基于pthread实现了互斥锁
+ * 
+*/
 
 class MutexLock{
 public:
@@ -34,10 +37,18 @@ public:
         holder_ = 0;
         pthread_mutex_unlock(&mutex_);
     }
+
+    pthread_mutex_t* GetPthreadMutex(){
+        return &mutex_;
+    }
 private:
     pid_t holder_;
     pthread_mutex_t mutex_;
 };
+
+/**
+ * @brief 使用RAII的风格实现了lock_guard类
+*/
 
 class MutexLockGuard{
 public:
@@ -54,6 +65,35 @@ private:
 };
 //禁止使用临时对象，如MutexLock(x)这样的形式
 #define MutexLockGuard(x) static_assert(0 == 1)
+
+
+/**
+ * @brief 实现条件变量
+*/
+
+class Condition{
+public:
+    explicit Condition(MutexLock &mutex) : mtx(mutex){
+        pthread_cond_init(&cond,NULL);
+    }
+
+    ~Condition(){
+        pthread_cond_destroy(&cond);
+    }
+    void wait(){
+        pthread_cond_wait(&cond,mtx.GetPthreadMutex());
+    }
+    void notify_one(){
+        pthread_cond_signal(&cond);
+    }
+    void notify_all(){
+        pthread_cond_broadcast(&cond);
+    }
+private:
+    MutexLock& mtx;
+    pthread_cond_t cond;
+};
+
 
 
 int main(){
