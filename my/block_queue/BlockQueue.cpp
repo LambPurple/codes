d08805562965312e0b54c96b3286a5c2f52ccc08
yzj <1790122327@qq.com>
2024-04-18 16:24:52 +0800

    补充了两种实现线程同步的方式

--- my/block_queue/BlockQueue.cpp
+++ my/block_queue/BlockQueue.cpp
@@ -1,5 +1,6 @@
 #include "BlockQueue.h"
 
+//使用了两个条件变量来实现生产者-消费者模型
 template <typename T>
 size_t BlockQueue<T>::size() const
 {
@@ -33,3 +34,74 @@ void BlockQueue<T>::put(const T &x)
     std::cout << "放下" << x << std::endl;
     consume.notify_one();
 }
+
+
+//一种不使用条件变量的方法 -> 使用自旋锁
+/*
+#define CAN_CONSUME (depth > 0)
+#define CAN_PRODUCE (depth < N)
+int N = 4;//容量是4
+int depth = 0;
+
+mutex mtx;
+
+//spin实现线程同步
+void produce(){
+retry:
+    unique_lock<std::mutex> lck(mtx);
+    if(!CAN_PRODUCE){
+        lck.unlock();
+        goto retry;
+    }
+    assert(CAN_PRODUCE);
+    printf("(");
+    ++ depth;
+}
+
+void consume(){
+retry:
+    unique_lock<std::mutex> lck(mtx);
+    if(!CAN_CONSUME){
+        lck.unlock();
+        goto retry;
+    }
+    assert(CAN_CONSUME);
+    printf(")");
+    -- depth;
+}
+
+*/
+
+/*
+#define CAN_CONSUME (depth > 0)
+#define CAN_PRODUCE (depth < N)
+int N = 4;//容量是4
+int depth = 0;
+
+mutex mtx;
+condition_variable cond;
+
+//采用spin的思想，使用一个条件变量，一个mutex实现线程同步
+void produce(){
+    unique_lock<std::mutex> lck(mtx);
+    while(!CAN_PRODUCE){
+        cond.wait(lck);
+    }
+    assert(CAN_PRODUCE);
+    printf("(");
+    ++ depth;
+    cond.notify_all();//每次修改需要唤醒所有睡眠中的线程
+}
+
+void consume(){
+    unique_lock<std::mutex> lck(mtx);
+    while(!CAN_CONSUME){
+        cond.wait(lck);
+    }
+    assert(CAN_CONSUME);
+    printf(")");
+    -- depth;
+    cond.notify_all();
+}
+
+*/
